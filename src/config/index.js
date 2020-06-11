465d33f0a3ef9dfbdd793e17e8ef1ca9ce03577f
arjun1709 <arjun1709@gmail.com>
2020-06-11 21:23:51 +0530

    Updated credentials to beta.

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  AUTH_SERVICE_URL: 'https://dev-auth.inblox.me',
+  AUTH_SERVICE_URL: 'https://auth.inblox.me',
   INFURA_KEY: 'c52ef01cba8b4e1d8aa98c12c64318a9',
   DEFAULT_GAS_LIMIT: 6000000,
 };
