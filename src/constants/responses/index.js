dbb98edc95ec93e961ea4c35783f308d8bbf3554
arjun1709 <arjun1709@gmail.com>
2020-06-16 18:02:22 +0530

    Added methods to import wallet.

--- src/constants/responses/index.js
+++ src/constants/responses/index.js
@@ -1,4 +1,7 @@
 module.exports = {
   INCORRECT_PASSWORD: 'Incorrect password.',
   TRANSACTION_ERROR: 'Transaction error. Please try after sometime.',
+  INVALID_PRIVATE_KEY: 'The private key entered is invalid.',
+  WRONG_PASSWORD: 'The password entered is incorrect.',
+  INVALID_MNEMONIC: 'Invalid mnemonic phrase.',
 };
