5df712c782573ef24b8de79d2ee7c690b1e76a55
Arjun Sasikumar <arjun@inblox.me>
2021-03-13 17:53:27 +0530

    Updated the base urls of the backend services.

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-  AUTH_SERVICE_URL_PROD: 'https://auth.inblox.me',
-  AUTH_SERVICE_URL_DEV: 'https://dev-auth.inblox.me',
-  AUTH_SERVICE_URL_TEST: 'https://test-auth.inblox.me',
+  AUTH_SERVICE_URL_PROD: 'https://auth.getsafle.com',
+  AUTH_SERVICE_URL_DEV: 'https://dev-auth.getsafle.com',
+  AUTH_SERVICE_URL_TEST: 'https://test-auth.getsafle.com',
   DEFAULT_GAS_LIMIT: 6000000,
   MAINNET_ETHERSCAN_URL: 'https://etherscan.io/tx',
   ROPSTEN_ETHERSCAN_URL: 'https://ropsten.etherscan.io/tx',
