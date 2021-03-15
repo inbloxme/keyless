278761b5afa1cec4a916790446d3b163822ff4ae
Arjun Sasikumar <arjun@inblox.me>
2021-03-15 13:34:51 +0530

    Updated the default gas limit to 40000

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   AUTH_SERVICE_URL_PROD: 'https://auth.getsafle.com',
   AUTH_SERVICE_URL_DEV: 'https://dev-auth.getsafle.com',
   AUTH_SERVICE_URL_TEST: 'https://test-auth.getsafle.com',
-  DEFAULT_GAS_LIMIT: 6000000,
+  DEFAULT_GAS_LIMIT: 40000,
   MAINNET_ETHERSCAN_URL: 'https://etherscan.io/tx',
   ROPSTEN_ETHERSCAN_URL: 'https://ropsten.etherscan.io/tx',
   RINKEBY_ETHERSCAN_URL: 'https://rinkeby.etherscan.io/tx',
